e98958988efae41d915f8ff7bd6cd64d85f4831c
govardhanmgr <morsu.gr000@gmail.com>
2022-10-20 00:42:01 +0530

    nfvjNzdv

---
+++ src/main/java/com/snadtraining/jpatutorial/entity/Student.java
@@ -0,0 +1,29 @@
+package com.snadtraining.jpatutorial.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private  Long studentId;
+    private  String firstName;
+    private  String lastName;
+    private String emailId;
+    private String guardianName;
+    private  String guardianEmail;
+    private  String guardianMobile;
+
+
+}
